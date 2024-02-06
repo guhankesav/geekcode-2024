9e1232a90ce6c91a93f22a2bb2d87eaf9b007a88
Guhan K <47454368+guhankesav@users.noreply.github.com>
2024-02-06 18:36:13 +0530

    Time: 53 ms (49.51%) | Memory: 17 MB (59.34%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
+        # Solution 1
+        if len(s) != len(t):
+            return False
 
-        #Solution 1
+        counterS, counterT = {}, {}
+
+        for i in range(len(s)):
+            counterS[s[i]] = 1 + counterS.get(s[i], 0)
+            counterT[t[i]] = 1 + counterT.get(t[i], 0)
+        for i in counterS:
+            if counterS[i] != counterT.get(i,0):
+                return False
+        return True
+
+        #Solution 2
         counterS, counterT = {}, {}
 
         for char in s:
@@ -12,31 +26,20 @@ class Solution:
 
         if len(counterS) != len(counterT):
             return False
-            
+
         for char in counterT:
             if counterT.get(char, -1) != counterS.get(char, -1):
                 return False
         
         return True
 
-        # Solution 2
+        # Solution 3
         return Counter(s) == Counter(t)
 
-        #Solution 3
+        #Solution 4
         return sorted(s) == sorted(t)
 
 
 
-        # if len(s) != len(t):
-        #     return False
 
-        # counterS, counterT = {}, {}
-
-        # for i in range(len(s)):
-        #     counterS[s[i]] = 1 + counterS.get(s[i], 0)
-        #     counterT[t[i]] = 1 + counterT.get(t[i], 0)
-        # for i in counterS:
-        #     if counterS[i] != counterT.get(i,0):
-        #         return False
-        # return True
         
