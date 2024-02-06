39337b38726313c49704c89448127ad6bec25a67
Guhan K <47454368+guhankesav@users.noreply.github.com>
2024-02-06 18:58:25 +0530

    Time: 59 ms (75.25%) | Memory: 17.9 MB (58.08%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,30 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Solution Brute Force  - Time Complexity O(n^2), Space Complecity - O(1)
+
+        """
+        Solution 1
+        Using hashMap
+        TimeComplexity - O(n)
+        spaceComplexity - O(n)
+        """
+        hashMap = {}
+        for index, value in enumerate(nums):
+
+            if (target - value) in hashMap:
+                return [index, hashMap[target - value]]
+                
+            hashMap[value] = index
+            
+
+
+
+        """
+        Solution 2
+        Brute Force  
+        Time Complexity  - O(n^2)
+        Space Complecity - O(1)
+        """
+
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
