d05460bbb18280852c26314bcffc298df63bffd7
Guhan K <47454368+guhankesav@users.noreply.github.com>
2024-08-25 02:01:06 +0530

    Time: 48 ms (29.13%) | Memory: 16.8 MB (40.87%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,23 +1,36 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
-        stack = []
 
-        for i in operations:
-            
-            if i not in ["+","D","C"]:
-                stack.append(i)
-            elif i == "+":
-                stack.append(str(int(stack[-2]) + int(stack[-1])))
-            elif i == "D":
-                stack.append(str(int(stack[-1])*2))
-            elif i == "C":
-                stack.pop()
+        stack = []
+        for operation in operations:
+            if operation == '+':
+                stack.append(stack[-1] + stack[-2])
+            elif operation == 'D':
+                stack.append(stack[-1]*2)
+            elif operation == 'C':
+                stack.pop(-1)
             else:
-                print("Error unknown")
-            print(stack)
-        tot = 0
-        for i in stack:
-            tot += int(i)
-        return tot
+                stack.append(int(operation))
+        return sum(stack)
+
+        # stack = []
+
+        # for i in operations:
+            
+        #     if i not in ["+","D","C"]:
+        #         stack.append(i)
+        #     elif i == "+":
+        #         stack.append(str(int(stack[-2]) + int(stack[-1])))
+        #     elif i == "D":
+        #         stack.append(str(int(stack[-1])*2))
+        #     elif i == "C":
+        #         stack.pop()
+        #     else:
+        #         print("Error unknown")
+        #     print(stack)
+        # tot = 0
+        # for i in stack:
+        #     tot += int(i)
+        # return tot
             
         
